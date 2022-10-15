aec131773fb0598b909eca7468e51dcf75c22721
apphot <apphot.contact@gmail.com>
2022-10-15 16:19:53 +0700

    1.1

--- LibTutorial/mylibrary/src/main/java/com/tutorial/mylibrary/ShowToast.java
+++ LibTutorial/mylibrary/src/main/java/com/tutorial/mylibrary/ShowToast.java
@@ -7,4 +7,8 @@ public class ShowToast {
     public void show(Context context, String msg) {
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
+
+    public void show1(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
 }
